ff5f6f575c17b797d5f5613c4cef3fc52dfcfe8c
Misael00103 <beriguetemisael@gmail.com>
2022-08-13 16:06:54 -0400

    first commit

---
+++ Ubicacion_GPS/Ubicacion_GPS/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace Ubicacion_GPS
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            if (!double.TryParse(txtLatitud.Text, out double latitud))
+                return;
+            if (!double.TryParse(txtLongitud.Text, out double longitud))
+                return;
+            await Map.OpenAsync(latitud, longitud, new MapLaunchOptions
+            {
+
+                Name = txtNombreUbicacion.Text,
+                NavigationMode = NavigationMode.None
+
+
+            });
+
+
+    } }
+}
